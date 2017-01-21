5f3866ef5f74a48a590ecfb4628caca84fa2dfd6
georgiStankov <georgi.stankov6@gmail.com>
2017-01-21 17:00:43 +0200

    Remove

--- Cars/src/main/java/bg/jwd/cars/service/user/UserService.java
+++
@@ -1,9 +0,0 @@
-package bg.jwd.cars.service.user;
-
-import java.util.List;
-
-import bg.jwd.cars.entity.user.AutoUser;
-
-public interface UserService {
-	List<AutoUser> getUsers(String username, String status);
-}
