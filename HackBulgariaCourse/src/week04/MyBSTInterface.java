6135b92dbf2d2bd6b294c93b581fed72657dc30c
georgiStankov <georgi.stankov6@gmail.com>
2017-01-21 17:01:53 +0200

    Remove

--- HackBulgariaCourse/src/week04/MyBSTInterface.java
+++
@@ -1,9 +0,0 @@
-package week04;
-
-public interface MyBSTInterface<T extends Comparable<T>> {
-
-	
-	public void insert(T element);
-	public void remove(T element);
-	public boolean find(T element);
-}
--- HackBulgariaCourse/src/week08/Axe.java
+++
@@ -1,9 +0,0 @@
-package week08;
-
-public class Axe extends ToSmash{
-
-	public Axe() {
-		super(10, 50);
-	}
-	
-}
